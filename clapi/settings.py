5b4e7473d21b2fb3eccb2a36bc5a36f60b9b34bf
Ubuntu <ubuntu@ip-10-0-5-33.eu-west-2.compute.internal>
2021-07-28 07:19:01 +0000

    getting it working

--- clapi/settings.py
+++ clapi/settings.py
@@ -1,12 +1,4 @@
-from pathlib import Path
 from .local_settings import *
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
 # Application definition
@@ -18,6 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
