faf2ab173b736788043635fa7f3694de1725d1d1
masystems <marco@masys.co.uk>
2021-07-28 08:06:13 +0100

    updated settings

--- clapi/settings.py
+++ clapi/settings.py
@@ -4,10 +4,8 @@ from .local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 
 # Application definition
 
